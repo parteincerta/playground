d98b1ed98baabe4ef4af4172d19975bea47f16bd
Fernando Moreira <fernando@parteincerta.com>
2024-10-27 20:31:09 +0000

    rt-weekend: Simplify 01.c implementation

--- c/rt-weekend/01.c
+++ c/rt-weekend/01.c
@@ -9,17 +9,9 @@ int main (int argc, char **argv) {
 	const int img_height = argc == 3 ? atoi(argv[2]) : 256;
 
 	// Writing the PPM header.
-	fputs("P3\n", stdout);
-	char dim_buffer[11] = {'\0'};
-	sprintf(dim_buffer,"%d", img_width);
-	fputs(dim_buffer, stdout);
-	fputs(" ", stdout);
-	sprintf(dim_buffer,"%d", img_height);
-	fputs(dim_buffer, stdout);
-	fputs("\n255\n", stdout);
+	fprintf(stdout, "P3\n%d %d\n%d\n", img_width, img_height, 255);
 
 	// Writing the PPM body.
-	char channel_buffer[11] = {'\0'};
 	for (int j = 0; j < img_height; j++) {
 	for (int i = 0; i < img_width; i++) {
 		const float r = (float)i / (img_width - 1);
@@ -28,15 +20,7 @@ int main (int argc, char **argv) {
 		const int int_r = (int)255.999f * r;
 		const int int_g = (int)255.999f * g;
 
-		sprintf(channel_buffer, "%d", int_r);
-		fputs(channel_buffer, stdout);
-		fputs(" ", stdout);
-		sprintf(channel_buffer, "%d", int_g);
-		fputs(channel_buffer, stdout);
-		fputs(" ", stdout);
-		sprintf(channel_buffer, "%d", 0);
-		fputs(channel_buffer, stdout);
-		fputs("\n", stdout);
+		fprintf(stdout, "%d %d %d\n", int_r, int_g, 0);
 	}}
 
 	return 0;
